cd7909c3269fb228401b742bfcc7b81c938e0b70
LeNgocVanAnh <anhle161108@gmail.com>
2022-04-03 16:07:20 +0700

    add web action pages

---
+++ src/test/java/corefw/testcore/WebAction.java
@@ -0,0 +1,4 @@
+package corefw.testcore;
+
+public class WebAction {
+}
---
+++ src/test/java/objects/pages/HomePage.java
@@ -0,0 +1,7 @@
+package objects.pages;
+
+import corefw.testcore.WebAction;
+
+public class HomePage extends WebAction {
+
+}
